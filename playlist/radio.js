39ba18321cf2583bdd9f6c6380f07423ac3df202
alessaSl <141650484+alessaSl@users.noreply.github.com>
2023-09-24 18:07:49 -0400

    Update and rename play.js to radio.js

---
+++ playlist/radio.js
@@ -0,0 +1,43 @@
+export default [
+    {
+      src: "ella.mp3",
+      name: "Stereo Love - Edward Maya & Mia Martina"
+    },
+    
+    {
+      src: "LADYGAGA.mp3",
+      name: "Peso Pluma - Lady Gaga",
+    },
+    {
+      src: "Mibello.mp3",
+      name: "Mi Bello - Natanael Cano",
+    },
+    {
+      src: "rosaPastel.mp3",
+      name: "Rosa Pastel - Peso Pluma",
+    },
+    {
+      src: "stereolove.mp3",
+      name: "Peso Pluma - Ella Baila Sola",
+    },
+    {
+      src: "YourLove.mp3",
+      name: "Your Love - Cavin Harris & Disciples",
+    },
+    {
+        src: "NaNaNa.mp3",
+        name: "Na Na Na - Akon",
+      },
+      {
+        src: "voodoo.mp3",
+        name: "Voodoo - UCLÃ",
+      },
+      {
+        src: "SENTIMENTOPURO.mp3",
+        name: "Sentimento Puro -  Sidoka",
+      },
+      {
+        src: "celembra.mp3",
+        name: "Será Que Cê Lembra? - Sidoka",
+      },
+  ];
